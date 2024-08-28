1084f9398444ee67acbe13f1ded42a2fb237dc99
sloev / Johannes Valbjørn <johannes.valbjorn@gmail.com>
2024-08-28 11:28:04 +0200

    l

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ console.log(roomName)
 let presentMode = params.get("present") == "true" ? true : false;
 var canvas = document.getElementById('qr')
 
-QRCode.toCanvas(canvas, window.location.href.split("?")[0]+`?${roomName}`, function (error) {
-  if (error) console.error(error)
-  console.log('success!');
+QRCode.toCanvas(canvas, window.location.href.split("?")[0] + `?${roomName}`, function (error) {
+    if (error) console.error(error)
+    console.log('success!');
 })
 const goHome = () => {
     window.open(window.location.href.split("?")[0], "_self")
@@ -206,9 +206,9 @@ if (presentMode) {
     var now = new Date() / 10.0
     let { canvas, context } = init("game");
     initPointer();
-initKeys();
+    initKeys();
     let qr = document.getElementById("qr")
-    qr.style.display="block"
+    qr.style.display = "block"
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -217,23 +217,22 @@ initKeys();
         canvases.push(offscreen)
 
     }
+    let fader = 0
     let loop = GameLoop({ // create the main game loop
         clearCanvas: true,
         update: function () { // update the game state
             const newNow = new Date() / 10.
-            if (events.length){// && newNow + 100 > now) {
+            if (events.length) {// && newNow + 100 > now) {
                 now = newNow + 100
-                const item = events.pop()
-                let c = canvases[canvas_index]
-            let ctx = c.getContext('2d')
-            ctx.globalAlpha = 0.1;
-    ctx.fillRect(0,0,c.width,c.height);
-    ctx.globalAlpha = 1.0;
+                const item = events[0]
+                events = events.slice(1)
+                fader = spritesheetUrls.length
+
 
                 getSprite(item.peerId, spritesheetUrls[item.spriteUrlIndex]).then(sprite => {
                     console.log("raw", item)
-                    sprite.x = (item.x*-1.0)*context.canvas.width+context.canvas.width/2;
-                    sprite.y = (item.y*-1.0)*context.canvas.height+context.canvas.height/2;
+                    sprite.x = (item.x * -1.0) * context.canvas.width + context.canvas.width / 2;
+                    sprite.y = (item.y * -1.0) * context.canvas.height + context.canvas.height / 2;
                     console.log(sprite.x, sprite.y)
                 })
 
@@ -244,7 +243,7 @@ initKeys();
 
             if (pointerPressed("left")) {
                 let m = getPointer()
-                const e = { spriteUrlIndex, peerId: room.selfId, x: m.x/context.canvas.width, y: m.ycontext.canvas.height }
+                const e = { spriteUrlIndex, peerId: room.selfId, x: m.x / context.canvas.width, y: m.ycontext.canvas.height }
                 shoot(e)
                 events.push(e)
 
@@ -264,21 +263,28 @@ initKeys();
             canvas_index++
             canvas_index = canvas_index % canvases.length
             let c = canvases[canvas_index]
+            if (fader > 0) {
+                let ctx = c.getContext('2d')
+                ctx.globalAlpha = 0.01;
+                ctx.fillRect(0, 0, c.width, c.height);
+                ctx.globalAlpha = 1.0;
+                fader-=1
+            }
 
-          
+
             context.drawImage(
                 c,
                 0,
-                0         );
+                0);
 
             for (let key of Object.keys(sprites)) {
                 let sprite = sprites[key].sprite
                 sprite.render()
             }
-      
+
             c.getContext('2d').drawImage(
                 canvas,
-                0,0
+                0, 0
             );
         }
     });
@@ -287,11 +293,11 @@ initKeys();
 } else if (roomName != "notinitialized") {
     document.getElementById("phone").style.display = "block"
     let initialAlpha, initialBeta;
-    document.getElementById("reset").onclick = ()=>{
-        initialAlpha=null
+    document.getElementById("reset").onclick = () => {
+        initialAlpha = null
         initialBeta = null
     }
-   
+
     let previewGif;
     let { canvasShoot, contextShoot } = init("shootbutton");
     let shootbuttoncanvas = document.getElementById("shootbutton")
@@ -301,17 +307,17 @@ initKeys();
     shootbuttoncanvas.height = expander.clientHeight;
     console.log(canvasShoot, contextShoot, "canvas")
 
-    document.getElementById("nextgif").onclick = ()=>{
+    document.getElementById("nextgif").onclick = () => {
         spriteUrlIndex = (spriteUrlIndex + 1) % spritesheetUrls.length
     }
-    document.getElementById("previousgif").onclick = ()=>{
+    document.getElementById("previousgif").onclick = () => {
         spriteUrlIndex = (spriteUrlIndex + spritesheetUrls.length - 1) % spritesheetUrls.length
     }
     let shootNow = false
-    shootbuttoncanvas.ontouchstart = ()=>{
+    shootbuttoncanvas.ontouchstart = () => {
         shootNow = true
     }
-    shootbuttoncanvas.ontouchend = ()=>{
+    shootbuttoncanvas.ontouchend = () => {
         shootNow = false
     }
     window.addEventListener('deviceorientation', async (event) => {
@@ -319,17 +325,17 @@ initKeys();
             initialAlpha = event.alpha
             initialBeta = event.beta
         }
-        var x = event.alpha-initialAlpha;  // In degree in the range [-180,180]
-  var y = event.beta-initialBeta; // In degree in the range [-90,90]
-  if(x<-180)x=-180
-  if(x>180)x=180
-  if(y<-90)y=-90
-  if(y>90)y=90
-  x = x/180.0
-  y=y/90
+        var x = event.alpha - initialAlpha;  // In degree in the range [-180,180]
+        var y = event.beta - initialBeta; // In degree in the range [-90,90]
+        if (x < -180) x = -180
+        if (x > 180) x = 180
+        if (y < -90) y = -90
+        if (y > 90) y = 90
+        x = x / 180.0
+        y = y / 90
 
         if (shootNow) {
-            const e = { spriteUrlIndex, peerId: room.selfId, x:x, alpha:event.alpha, y:y, beta:event.beta }
+            const e = { spriteUrlIndex, peerId: room.selfId, x: x, alpha: event.alpha, y: y, beta: event.beta }
             console.log(e)
             shoot(e)
         }
@@ -339,17 +345,17 @@ initKeys();
         clearCanvas: true,
         update: function () { // update the game state    
             getSprite("preview", spritesheetUrls[spriteUrlIndex]).then(sprite => {
-                sprite.x = shootbuttoncanvas.width/3 + shootbuttoncanvas.width/6;
-                sprite.y = shootbuttoncanvas.height/2;
-                sprite.width = expander.clientWidth/2;
-                sprite.height = expander.clientHeight/2;
+                sprite.x = shootbuttoncanvas.width / 3 + shootbuttoncanvas.width / 6;
+                sprite.y = shootbuttoncanvas.height / 2;
+                sprite.width = expander.clientWidth / 2;
+                sprite.height = expander.clientHeight / 2;
                 previewGif = sprite
-                
+
 
             })
-            
+
             if (!!previewGif) {
-previewGif.update()
+                previewGif.update()
             }
         },
         render: function () { // render the game state
